2ab157ab0996cbea2cd70574ceec7c231d7b5875
M Wang <m.h.wang@live.com>
2021-08-19 23:09:22 -0400

    update msigdb.gsea function default parameters

--- R/msigdb.gsea.R
+++ R/msigdb.gsea.R
@@ -1,6 +1,6 @@
-msigdb.gsea=function(x, query.population=NULL, genesets=c('C2.CP','C5.BP','C5.CC','C5.MF'),
- background='common', name.x='Input', name.go='MSigDB', method=c('hypergeometric','GSEA','logitreg'),
- adj="BH", species=c('human','mouse'), ReportOverlap=TRUE, ncores=1, gsea.alpha=1, permutations=ifelse(method=='GSEA',1000,0), iseed=12345){
-	go=msigdb.genesets(sets=genesets,type='symbols',species=species,return.data.frame=TRUE)
+msigdb.gsea=function(x, query.population=NULL, genesets=c('c2.cp', 'c5.go.bp', 'c5.go.cc', 'c5.go.mf'),
+ background='common', name.x='Input', name.go='MSigDB', method=c('hypergeometric', 'GSEA', 'logitreg'),
+ adj="BH", species=c('human','mouse'), ReportOverlap=TRUE, ncores=1, gsea.alpha=1, permutations=ifelse(method=='GSEA', 1000, 0), iseed=12345){
+	go=msigdb.genesets(sets=genesets, type='symbols', species=species, return.data.frame=TRUE)
 	GOtest(x=x,go=go,query.population=query.population,background=background,name.x=name.x,name.go=name.go,method=method,adj=adj,ReportOverlap=ReportOverlap,ncores=ncores,gsea.alpha=gsea.alpha,permutations=permutations, iseed=iseed)
 }
